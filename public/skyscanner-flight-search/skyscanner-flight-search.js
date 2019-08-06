e5ab8470ce0831d8218505fb4f9f0aa55411359b
Demetrio Carvalho <resource.dcarvalho@bv.com.br>
2019-08-06 14:24:31 -0300

    separate html from javascript

--- public/skyscanner-flight-search/skyscanner-flight-search.js
+++ public/skyscanner-flight-search/skyscanner-flight-search.js
@@ -1,30 +1,41 @@
-const template = document.createElement("template");
-template.innerHTML = `<div id="firstdiv"><input id="inputSessionKey"/></div>`;
+// const template = document.createElement("template");
+// template.innerHTML = `<div id="firstdiv"><input id="inputSessionKey"/></div>`;
 
-class skyscannerFlightSearch extends HTMLElement {
-  constructor() {
-    super();
+(async () => {
+  const res = await fetch(
+    "/skyscanner-flight-search/skyscanner-flight-search.html"
+  );
+  const textTemplate = await res.text();
+
+  const HTMLTemplate = new DOMParser()
+    .parseFromString(textTemplate, "text/html")
+    .querySelector("template");
+
+  class skyscannerFlightSearch extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    connectedCallback() {
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.appendChild(HTMLTemplate.content.cloneNode(true));
+
+      const inputSessionKey = this.shadowRoot.getElementById("inputSessionKey");
+      const url = "http://localhost:8080/getsessionkey";
+
+      fetch(url)
+        .then(function(body) {
+          return body.text();
+        })
+        .then(function(data) {
+          inputSessionKey.value = data;
+          console.log(data);
+        });
+    }
   }
 
-  connectedCallback() {
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-    const inputSessionKey = this.shadowRoot.getElementById("inputSessionKey");
-    const url = "http://localhost:8080/getsessionkey";
-
-    fetch(url)
-      .then(function(body) {
-        return body.text();
-      })
-      .then(function(data) {
-        inputSessionKey.value = data;
-        console.log(data);
-      });
-  }
-}
-
-window.customElements.define(
-  "skyscanner-flight-search",
-  skyscannerFlightSearch
-);
+  window.customElements.define(
+    "skyscanner-flight-search",
+    skyscannerFlightSearch
+  );
+})();
